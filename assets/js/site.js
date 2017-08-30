d7bad955c67e5a74bd41bc085185a045876cf572
Johnathan Lyman <jelyman@gmail.com>
2017-08-29 22:17:38 -0700

    Allow for images wider than paragraphs

--- assets/js/site.js
+++ assets/js/site.js
@@ -8,3 +8,9 @@ var position = scrollPercent;
 $("#progressbar").attr('value', position);
 
 });
+
+// Images meant to be wider than the text should have their parent paragraph element extend to match.
+$('article>div>p:has(img[src*="med"])').addClass('med');
+
+// Images meant go all super mega ultra will fill the page width.
+$('article>div>p:has(img[src*="large"])').addClass('large');
